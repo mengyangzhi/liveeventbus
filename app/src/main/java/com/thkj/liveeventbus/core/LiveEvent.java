a3709cd74173cc2fce592856e2d53623d7c5e2e6
myz <574622014@qq.com>
2024-03-29 20:19:58 +0800

    Initial commit

---
+++ app/src/main/java/com/thkj/liveeventbus/core/LiveEvent.java
@@ -0,0 +1,9 @@
+package com.thkj.liveeventbus.core;
+
+import java.io.Serializable;
+
+/**
+ * Created by liaohailiang on 2020-05-30.
+ */
+public interface LiveEvent extends Serializable {
+}
---
+++ app/src/main/java/com/thkj/liveeventbus/ipc/core/ParcelableProcessor.java
@@ -0,0 +1,26 @@
+package com.thkj.liveeventbus.ipc.core;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+
+import com.thkj.liveeventbus.ipc.consts.IpcConst;
+
+/**
+ * Created by liaohailiang on 2019/5/30.
+ */
+public class ParcelableProcessor implements Processor {
+
+    @Override
+    public boolean writeToBundle(Bundle bundle, Object value) {
+        if (!(value instanceof Parcelable)) {
+            return false;
+        }
+        bundle.putParcelable(IpcConst.KEY_VALUE, (Parcelable) value);
+        return true;
+    }
+
+    @Override
+    public Object createFromBundle(Bundle bundle) {
+        return bundle.getParcelable(IpcConst.KEY_VALUE);
+    }
+}
